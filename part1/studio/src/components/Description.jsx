9e3cadfc08e6b9c021a9c47ba8d339708459eb71
Misael Barcenas <mbarcenas@Misaels-Air.attlocal.net>
2023-11-13 14:09:12 -0600

    started studio

---
+++ part1/studio/src/components/Description.jsx
@@ -0,0 +1,33 @@
+import styles from './Description.module.css';
+import React from 'react';
+
+function RecipeAuthor() {
+    let authorLink = "https://joyfoodsunshine.com/about-laura/";
+    let authorPhoto = "https://joyfoodsunshine.com/wp-content/uploads/2023/10/Laura_Headshot.jpeg";
+    let authorName = "Laura"
+    return (
+        <div className={styles.recipeAuthorBlock}>
+            <img src={authorPhoto} alt="Reasonable alt text" className={styles.imageUpdates} />
+            <div>
+                <h3>{authorName}</h3>
+                <a href={authorLink}>Blog name</a>
+            </div>
+        </div>
+    );
+};
+
+class RecipeDescription extends React.Component {
+    render() {
+        return (
+        <div>
+            <div>
+                <h1>Recipe Title</h1>
+                <p>Short recipe description</p>
+            </div>
+            <RecipeAuthor />
+        </div>)
+        
+     }
+}
+
+export default RecipeDescription;

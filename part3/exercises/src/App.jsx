c491e1f5dbb8e8a25da3dafbc487cff0f7f7a74e
Misael Barcenas <mbarcenas@Misaels-MacBook-Air.local>
2023-11-13 20:25:05 -0600

    started exercises

--- part3/exercises/src/App.jsx
+++ part3/exercises/src/App.jsx
@@ -1,16 +1,15 @@
-import { useState } from 'react'
+// import { useState } from 'react'
 import './App.css'
 import HobbyIntroduction from './components/Introduction';
-import MyProjects from './components/Projects';
+// import MyProjects from './components/Projects';
 
 function App() {
 
   return (
     <>
       <div className="App">
-	  <h1>My Hobby: Needlepoint</h1>
-	  <HobbyIntroduction />
-	  <MyProjects />
+        <h1>My Hobby: Soccer</h1>
+        <HobbyIntroduction />
       </div>
     </>
   )
--- part3/exercises/src/components/Introduction.jsx
+++ part3/exercises/src/components/Introduction.jsx
@@ -1,11 +1,11 @@
 export default function HobbyIntroduction () {
    return(
       <div>
-         <h2>3 Fun Facts About Needlepoint</h2>
+         <h2>3 Fun Facts About this Hobby”</h2>
          <ol>
-            <li>Needlepoint uses a specialized canvas as it's base.</li>
-            <li>The most basic stitch is called a tent stitch.</li>
-            <li>Needlepoint uses a lot of different kinds of fibers.</li>
+            <li>Soccer is the most popular game in the world. In many countries it is known as “football”.</li>
+            <li>Earliest versions of the game can be traced back 3,000 years.</li>
+            <li>World Cup is every 4 years. The next one will be played in the USA, Mexico, and Canada</li>
          </ol>
       </div>
    );

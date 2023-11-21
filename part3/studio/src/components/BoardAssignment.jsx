1b88c5c8b02792d072c6e93d4d0aaa717f55ec80
Misael Barcenas <mbarcenas@Misaels-MacBook-Air.local>
2023-11-20 18:40:58 -0600

    studio

--- part3/studio/src/components/BoardAssignment.jsx
+++ part3/studio/src/components/BoardAssignment.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 
 export default function BoardAssignment () {
-   const boards = [];
+   const boards = [
+      { "label": "Thanksgiving Dinner dessert", "value": "Thanksgiving Dinner dessert" },
+      { "label": "Fourth of July Barbecue dessert", "value": "Fourth of July Barbecue dessert" },
+      { "label": "Party dessert", "value": "Party dessert" }
+   ];
+
+   const listOfBoards = (props) => {
+      return props.map((boardValue) => <option value={boardValue.value}> {boardValue.label }</option >);
+   };
+
+   const [boardName, setName] = useState('no boards yet');
+
 
    const handleChange = (event) => {
+      setName(event.target.value);
    }
 
    return (
       <div style={{paddingTop: "50px"}}>
       <label>Save to Board: </label>
-      <select value={boardName} onChange={handleChange}>
+         <select value={boardName} onChange={handleChange}>
+            <option value="">* Select A Board *</option>
+            {listOfBoards(boards)}
       </select>
 
       <p>Saved to {boardName}!</p>
--- part3/studio/src/components/Recipe.jsx
+++ part3/studio/src/components/Recipe.jsx
@@ -1,32 +1,33 @@
 import { useState } from 'react';
 
 const RecipeAuthor = () => {
-   let authorLink = "";
-   let authorPhoto = "";
-   let authorName = "";
+   let authorLink = "https://natashaskitchen.com/about/";
+   let authorPhoto = "https://natashaskitchen.com/wp-content/uploads/2023/02/NK-Head-Red-Shirt-small.png";
+   let authorName = "Natasha Kravchuk";
 
    return (
       <div>
-         <img src={authorPhoto} alt = "" style={{objectFit: "contain", borderRadius: "50%"}} />
+         <img src={authorPhoto} alt= "Natasha Kravchuk" style={{objectFit: "contain", borderRadius: "50%"}} />
          <div>
             <h3>{authorName}</h3>
-            <a href={authorLink}></a> 
+            <a href={authorLink}> Website</a> 
          </div>
       </div>
    );
 }
 
 const RecipeIngredients = () => {
-   const ingredients = [];
+   const ingredients = ["Granny Smith Apple", "Cinnamon", "All-purpose flour", "Unsalted butter", "Eggs", "granulated sugar"];
    return(
       <div>
-         <h3>Recipe Ingredients</h3>
+         <h3>Recipe Ingredients: </h3>
          <ul>
             <li>{ingredients[0]}</li>
             <li>{ingredients[1]}</li>
             <li>{ingredients[2]}</li>
             <li>{ingredients[3]}</li>
             <li>{ingredients[4]}</li>
+            <li>{ingredients[5]}</li>
          </ul>
       </div>
    );
@@ -36,8 +37,9 @@ const RecipeDescription = () => {
    return (
       <div> 
          <div>
-            <h1></h1>
-            <p></p>
+            <h1>Apple Pie Recipe</h1>
+            <p>Classic Apple Pie Recipe with an irresistible homemade apple pie filling. From the best flaky pie crust
+               to the generous saucy center, this recipe always gets glowing reviews</p>
          </div>
          <div className="recipePhotoBlock">
             <RecipeIngredients />
@@ -49,7 +51,7 @@ const RecipeDescription = () => {
 
 const RecipePhoto = () => {
    return (
-      <img src="" alt="" className="imageUpdates"/>
+      <img src="https://natashaskitchen.com/wp-content/uploads/2019/10/Apple-Pie-2.jpg" alt="Apple Pie" className="imageUpdates" style={{width:450}}/>
    );
 }
 
